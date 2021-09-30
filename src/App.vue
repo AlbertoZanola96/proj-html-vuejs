e2a9831a1194d4573e60260432336984b28bb15e
alberto <albertozanola1996@gmail.com>
2021-09-30 20:03:11 +0200

    Inizio header

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,14 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+#app {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,58 @@
 <template>
   <header>
-
+    <div class="container">
+      <div class="hed-top">
+        <div class="logo">
+        <img src="../assets/img/dark-logo.png" alt="">
+        </div>
+        <div class="serch">
+          <ul>
+            <li>
+              <a href="#">
+                <p>Home <i class="fas fa-angle-down"></i></p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <p>Pages <i class="fas fa-angle-down"></i></p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <p>Courses <i class="fas fa-angle-down"></i></p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <p>Features <i class="fas fa-angle-down"></i></p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <p>Blog <i class="fas fa-angle-down"></i></p>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <p>Shop <i class="fas fa-angle-down"></i></p>
+              </a>
+            </li>
+            <li>
+              <i class="fas fa-shopping-cart"></i>
+            </li>
+            <li>
+              <i class="far fa-user-circle"></i>
+            </li>
+            <li>
+              <input type="text" name="" id="">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="jumbotron">
+        
+      </div>
+    </div>
   </header>
 </template>
 
@@ -12,5 +64,51 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import '~@fortawesome/fontawesome-free/css/all.min.css';
+header {
+  width: 100%;
+  height: 750px;
+  background-color: #E9E6E3;
+
+  .container {
+    width: 60%;
+    height: 100%;
+    margin: 0 auto;
+    border: 1px solid black;
+    
+    .hed-top {
+      width: 100%;
+      height: 80px;
+      background-color: red;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .logo {
+        float: left;
+
+
+        img {
+          width: 170px;
+        }
+      }
+
+      .serch {
+        float: right;
+        display: flex;
+
+
+        ul {
+          display: flex;
+          list-style: none;
+
+          li {
+            margin-left: 35px;
+          }
+        }
+      }
+    }
+  }
+}
 
 </style>
